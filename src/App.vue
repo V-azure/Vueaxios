b07e95fa6df98369ffa338ce16f41968ada9dd5e
v-azure <2265573522@qq.com>
2022-09-28 16:14:05 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <router-view v-if="isRouterAlive"></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  data(){
+    return{
+      isRouterAlive:true
+    }
+  },
+  components: {
+  },
+  // 挂在
+  provide(){
+    return{
+      reload:this.reload
+    }
+  },
+  methods:{
+    reload(){
+      this.isRouterAlive = false;
+      this.$nextTick(() =>{
+        // DOM更新
+        this.isRouterAlive = true;
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
